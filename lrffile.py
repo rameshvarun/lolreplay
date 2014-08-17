b649ab3120531791f0bc27bed82d213ecede5f47
Varun Ramesh <varun.ramesh@aol.com>
2014-08-17 00:07:01 -0700

    Can now access the unencrypted, unzipped chunk and keyframe data.

--- lrffile.py
+++ lrffile.py
@@ -3,12 +3,24 @@ import json
 from cStringIO import StringIO
 import os
 import logging
+import base64
+import gzip
+
+from Crypto.Cipher import Blowfish
+
 
 '''
 The code for this module was strongly based on the following ruby
 script by https://github.com/robertabcd
 Script Link: https://github.com/robertabcd/lol-ob/blob/master/lrf.rb
 '''
+
+'''
+PKCS5 padding formula, from
+https://gist.github.com/crmccreary/5610068
+'''
+unpad = lambda s : s[0:-ord(s[-1])]
+
 
 class LRFFile:
   """Class that can be used to parse the information in a .lrf replay file"""
@@ -25,6 +37,14 @@ class LRFFile:
 
     # Meta data is a large json
     self.metadata = json.loads(self.file.read(self.meta_size))
+    self.gameid = self.metadata['matchID']
+
+    # Cipher required to decrypt data
+    self.encryptedKey = base64.b64decode(self.metadata['encryptionKey'])
+
+    gameidCipher = Blowfish.new(str(self.gameid), Blowfish.MODE_ECB)
+    self.key = unpad(gameidCipher.decrypt(self.encryptedKey))
+    self.cipher = Blowfish.new(self.key, Blowfish.MODE_ECB)
 
     # Store the current position as where the data section starts
     data_offset = self.file.tell()
@@ -38,12 +58,12 @@ class LRFFile:
 
       self.file.seek(data_offset + offset)
       if key == "stream":
-        self.parts[key] = LRFStream(self.file)
+        self.parts[key] = LRFStream(self.file, self.cipher)
       else:
         raise Exception("Unkown key %s in dataIndex list." % key)
 
 class LRFStream:
-  def __init__(self, file):
+  def __init__(self, file, cipher):
 
     self.type = file.read(1).encode("hex")
     self.size = struct.unpack("<L", file.read(4))[0]
@@ -51,31 +71,37 @@ class LRFStream:
     if self.type != '4e':
       raise Exception("Unkown stream type 0x%s" % self.type)
 
-    print type
-    print self.size
-
+    # Calculate the position of the end of this stream
     stream_end = file.tell() + self.size - 5
 
     payload_size = struct.unpack("<L", file.read(4))[0]
-    print payload_size
     assert (payload_size + 4) == (self.size - 5)
 
-    i = 0
+    # Read segments until the stream is over
     while file.tell() < stream_end:
-      unk0, uri_size = struct.unpack("<LL", file.read(8))
-      #print unk0
-      #print uri_size
-      obj = file.read(uri_size)
-      magic = file.read(1).encode("hex")
+      request = self.read_segment(file)
+      response = self.read_segment(file)
 
-      if magic != '0a':
-        raise Exception("Magic error: expected 0x0a got 0x%s" % magic)
+      print request
 
-      i = i + 1
-      if i % 2 == 1:
-        print obj
-      #quit()'''
+      if "getGameDataChunk" in request or "getKeyFrame" in request:
+        decrypted = unpad(cipher.decrypt(response))
+        gzip_input = StringIO(decrypted)
 
+        gzip_file = gzip.GzipFile(fileobj=gzip_input)
+        unzipped = gzip_file.read()
+        print unzipped
 
+  def read_segment(self, file):
+    unk0, segment_size = struct.unpack("<LL", file.read(8))
+    segment = file.read(segment_size)
+
+    ''' Every segment ends with a newline character. This helps make
+    sure we haven't read garbage binary and jumped somwhere'''
+    magic = file.read(1).encode("hex")
+    if magic != '0a':
+      raise Exception("Magic error: expected 0x0a got 0x%s" % magic)
+
+    return segment
 
 LRFFile("test.lrf")
